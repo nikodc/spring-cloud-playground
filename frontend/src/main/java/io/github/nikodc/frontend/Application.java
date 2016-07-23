6b21a7e138489e8c7a05ddd17e22e4f106b06e52
nikodc <nikodc2@gmail.com>
2016-07-23 17:01:42 -0300

    Agrego Zuul al frontend y configuro reglas básicas

--- frontend/src/main/java/io/github/nikodc/frontend/Application.java
+++ frontend/src/main/java/io/github/nikodc/frontend/Application.java
@@ -5,24 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
+@EnableZuulProxy
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-    @RequestMapping("/")
-    @ResponseBody
-    public ResponseEntity<String> index() {
-        return new ResponseEntity<>("OK", HttpStatus.OK);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
